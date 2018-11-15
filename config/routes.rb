7633b987b0d256db9a633783b4cd42994736a812
Natdenice <Clad1987@hotmail.com>
2018-11-15 15:12:42 +0100

    Modify

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  match '/employment', to: 'static_pages#employment', via: 'get'
-  match '/students', to: 'static_pages#students', via: 'get'
+  match 'employment', to: 'static_pages#employment', via: 'get'
+  match 'students', to: 'static_pages#students', via: 'get'
 end
