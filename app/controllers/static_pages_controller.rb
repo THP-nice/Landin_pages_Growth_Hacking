901af424b97ef26d2fac7af135643c595bf22c3f
Nikita Vasilev <vassilev.nikita@gmail.com>
2018-11-15 22:25:33 +0100

    gibbon

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,4 +7,20 @@ class StaticPagesController < ApplicationController
 
   def city
   end
+
+  def subscribe
+    @list_id = Rails.application.credentials.dig(:listid)
+  gibbon = Gibbon::Request.new
+
+ gibbon.lists(@list_id).members.create(
+   body: {
+     email_address: params[:email][:address],
+     status: "subscribed"
+   }
+ )
+
+ respond_to do |format|
+   format.json{render :json => {:message => "You have been Successfully added to the list! :)"}}
+     end
+   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   match 'employment', to: 'static_pages#employment', via: 'get'
   match 'city', to: 'static_pages#city', via: 'get'
+  post '/subscribe', to: 'static_pages#subscribe'
 end
