a897c2f0e77c1bc9a5b9a7ee98c67adc28f16005
Nikita Vasilev <vassilev.nikita@gmail.com>
2018-11-16 01:42:30 +0100

    form + typo + favicon

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -10,17 +10,17 @@ class StaticPagesController < ApplicationController
 
   def subscribe
     @list_id = Rails.application.credentials.dig(:listid)
-  gibbon = Gibbon::Request.new
+    gibbon = Gibbon::Request.new
 
- gibbon.lists(@list_id).members.create(
-   body: {
-     email_address: params[:email][:address],
-     status: "subscribed"
-   }
- )
+    gibbon.lists(@list_id).members.create(
+      body: {
+      email_address: params[:email][:address],
+      status: "subscribed"
+      }
+    )
 
- respond_to do |format|
-   format.json{render :json => {:message => "You have been Successfully added to the list! :)"}}
-     end
-   end
+    respond_to do |format|
+      format.json{render :json => {:message => "You have been Successfully added to the list! :)"}}
+    end
+  end
 end
